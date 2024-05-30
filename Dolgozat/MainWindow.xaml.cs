a2acd131cb6b1ad3faa2c5477306a701b12dd7be
norbl <norbl@DESKTOP-1MR6R6U>
2024-05-30 06:58:43 +0200

    Add project files.

---
+++ Dolgozat/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StackPanel0_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowContent(UserControl control)
+        {
+            ContentArea.Content = control;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowContent(new UserControl1());
+        }
+    }
+}
